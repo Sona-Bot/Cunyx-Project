58472252e216d5745d53001f6783cfd3d2b39657
Naz <mondal.nazib@gmail.com>
2016-10-16 22:20:38 -0400

    cunyx process creation update
    Every accepted input spawns a process (later on we will filter the input for acceptable commands and spawn processes based on those commands)

--- cunyx.java
+++ cunyx.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
 public class cunyx {
 	/** Main Method/Driver */
-		public static void main (String[] args){
-			
+		public static void main (String[] args){	
 			//Creating an input field
 			Scanner ui = new Scanner(System.in);
-			String input = "nope";
+			String input = null;
 			
 			//Infinite Loop while input is NOT "quit"
 			while (!terminationCheck(input)){
 				System.out.print("prompt> ");
 				input = ui.nextLine();
-				System.out.println("Your Input: " + input);
+				Process p = new Process();
+				p.setProcessName(input);
+				System.out.println(p.toString());
 			}
 			
 			System.out.println("Have a Good day!");
@@ -20,6 +21,5 @@ public class cunyx {
 	public static boolean terminationCheck (String message){
 		return message.equals("quit");
 	}
-	
 	
 }
